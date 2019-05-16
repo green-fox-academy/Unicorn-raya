ccea47151878d311d86c8948159785efdef829d6
ray <ruizhang70@qq.com>
2019-05-16 15:01:10 +0800

    except the last and optional not done yet, others are finished

--- week-02/day-03/json2xml.py
+++ week-02/day-03/json2xml.py
@@ -5,14 +5,14 @@ xml_file = "C:\\Users\\Ray_Zhang\\greenfox\\Unicorn-raya\\week-02\\day-03\\flowe
 
 def json2xml(json_file,xml_file):
     pass
-    fr=open(json_file,"r")  #打开json文件
-    ls=json.load(fr)  #将json格式的字符串转换成python的数据类型，解码过程
-    data=[ list(ls[0].keys()) ]  #获取列名,即key
+    fr=open(json_file,"r")  
+    ls=json.load(fr)  
+    data=[ list(ls[0].keys()) ]  
     for item in ls:
-        data.append(list(item.values()))  #获取每一行的值value
-    fr.close()  #关闭json文件
+        data.append(list(item.values()))  
+    fr.close()  
 
-    fw=open(xml_file,"w+")  #打开csv为文件
+    fw=open(xml_file,"w+")  
     for line in data:
         fw.write(",".join(line)+"\n")  #以逗号分隔一行的每个元素，最后换行
     fw.close()  #关闭csv文件
